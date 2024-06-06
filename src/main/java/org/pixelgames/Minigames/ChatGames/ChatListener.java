91ffe494592a91861d858c28aff885abc5c1b8f7
Wilkozx <wilkoisontheceiling@gmail.com>
2024-06-06 17:49:05 +0100

    created chat listener

---
+++ src/main/java/org/pixelgames/Minigames/ChatGames/ChatListener.java
@@ -0,0 +1,42 @@
+package org.pixelgames.Minigames.ChatGames;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.fml.common.Mod;
+import org.pixelgames.PixelGames;
+
+@Mod.EventBusSubscriber(modid = PixelGames.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ChatListener {
+    private static MinecraftServer server = null;
+    private static ChatListener instance = null;
+    private static boolean isMonitoringChat = false;
+    private static String toMatch = "";
+
+    public static void stopChatMonitoring() {
+        isMonitoringChat = false;
+    }
+
+    public static void startChatMonitoring() {
+        isMonitoringChat = true;
+    }
+
+    public static Boolean isMonitoringChat() {
+        return isMonitoringChat;
+    }
+
+    public static String getToMatch() {
+        return toMatch;
+    }
+
+    public static void setToMatch(String toMatch) {
+        ChatListener.toMatch = toMatch;
+    }
+
+    public static MinecraftServer getServer() {
+        return server;
+    }
+
+    public static void setMinecraftServer(MinecraftServer server) {
+        ChatListener.server = server;
+    }
+
+}
